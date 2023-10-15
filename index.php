267f9fda16dd9b201a73f35e83d7db231093b29b
ShadowFlyX <lascot.lexa@mail.ru>
2023-10-15 14:14:26 +0300

    first_version

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
 <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Schedule</title>
+</head>
+<body>
+    <header>
+        <div class="header-row">
+            <p>Расписание для студентов</p>
+        </div>
+    </header>
+    <div class="wrapper">
+
+    </div>
+</body>
+</html>
